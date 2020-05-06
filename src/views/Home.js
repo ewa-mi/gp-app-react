34196492c936c5c4235a14396d28c3c9afde6685
ewami <efili1989@gmail.com>
2020-05-06 14:02:08 +0200

    home setup - time, buttons

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
+  const now = new Date();
+  let timecheck = now.getHours() > 8 && now.getHours() < 17 ? "open" : "closed";
+
   return (
     <div>
-      <h2>this is home page</h2>
+      <h1>Welcome to AMS GPs</h1>
+      <br></br>
+      <p>
+        We are: <span className="timecheck">{timecheck}</span>
+      </p>
+      <p>To make an appointment </p>
+      <p> call: 020 555 5555</p>
+      <br></br>
+      <div className="buttonSection">
+        <Link className="click" to={`/doctor-schedule`}>
+          Who is on duty?
+        </Link>{" "}
+        <span></span>
+        <Link className="click" to={`/patient-signup`}>
+          I am a new patient
+        </Link>
+      </div>
     </div>
   );
 }
